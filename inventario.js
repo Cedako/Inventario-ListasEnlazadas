b745bd44d7565246c3d03ad38dc4e827c119d07f
Joshua <mramos29@ucol.mx>
2021-10-19 21:56:44 -0500

    listas enlazadas

--- inventario.js
+++ inventario.js
@@ -1,169 +1,150 @@
-export default class Inventario {
+export default class Inventario{
     constructor(table){
-        this._inventario = new Array();
+        this._inicio = null
         this._table = table
     }
 
-    //Encontrar posicion del producto dentro del vector en base a su código y devolverlo.
-    _findPos(product){
-        for(let i=0;i<this._inventario.length;i++){
-            if(product.getId()<this._inventario[i].getId()){
-                return i
-            }
-        }
-    }
-    //añadir producto al vector
+    //añadir producto a la lista
     add(product){
-        if(this._inventario.length<20){
-            let pos = this._find(product);
-            if (pos>=0){
-                return false;
-            }
-            else{
-                this._inventario.push(product);
-                //colocación del producto en orden ascendente.
-                if(this._inventario.length>1){
-                    let realPos = this._findPos(product);
-                    let j = (this._inventario.length)-1
-                    let k = (this._inventario.length)-2;
-                    for(let i=j; i>realPos; i--,k--){
-                    let temp = this._inventario[k];
-                    this._inventario[k] = this._inventario[i]
-                    this._inventario[i] = temp;
-                }
-                }
-                console.log(this._inventario.length);
+        let count = this._count()
+        let pos = this._find(product);
+        if(pos == null && count<20){
+            if (this._inicio==null){
+                this._inicio=product;
+                console.log(this._inicio);
+                return true;
+            } else {
+                let aux=this._inicio;
+                while(aux._siguiente!=null)
+                    aux=aux._siguiente;
+                aux._siguiente=product;
+                console.log(this._inicio);
                 return true;
             }
-        }
-        else{
-            return alert("El inventario está lleno.")
+        } else {
+            return false;
         }
     }
 
-    //Encontrar el producto en el vector
-    _find(product){
-        let pos = -1
-        for(let i=0; i<this._inventario.length; i++){
-            let s = this._inventario[i].getId()
-            if(s == product.getId()){
-                pos = i;
-            }
+    //Contar objetos en la lista
+    _count(){
+        let numObj = 1
+        let temp = this._inicio
+        while(temp != null){
+            numObj++
+            temp = temp._siguiente
         }
-        return pos;
+        console.log(numObj)
+        return numObj;
+    }
+    //Encontrar el producto en la lista
+    _find(product){
+        let temp = this._inicio
+        while(temp!=null){
+            if(temp._id == product.getId()){
+                return temp
+            }
+            temp = temp._siguiente
+        }
+        return null;
     }
 
     showAll(){
-        this._table.innerHTML = ""
-        for(let i=0; i<this._inventario.length;i++){
+        this._table.innerHTML = "";
+        let aux = this._inicio;
+        while (aux!==null){
             let row = this._table.insertRow(-1);
 
-            let colId = row.insertCell(0);
-            let colName = row.insertCell(1);
-            let colQuantity = row.insertCell(2);
-            let colCost = row.insertCell(3);
-            let colTotal = row.insertCell(4);
+            let col = row.insertCell(0);
 
-            colId.innerHTML = this._inventario[i].getId();
-            colName.innerHTML = this._inventario[i].getName();
-            colQuantity.innerHTML = this._inventario[i].getQuantity();
-            colCost.innerHTML = this._inventario[i].getCost();
-            colTotal.innerHTML = this._inventario[i].getTotal();
+            col.innerHTML = aux._info
+            aux=aux._siguiente
         }
     }
     showInverse(){
         this._table.innerHTML = "";
-        for(let i=0; i<this._inventario.length;i++){
+        let aux = this._inicio;
+        while (aux!==null){
             let row = this._table.insertRow(0);
 
-            let colId = row.insertCell(0);
-            let colName = row.insertCell(1);
-            let colQuantity = row.insertCell(2);
-            let colCost = row.insertCell(3);
-            let colTotal = row.insertCell(4);
+            let col = row.insertCell(0);
 
-            colId.innerHTML = this._inventario[i].getId();
-            colName.innerHTML = this._inventario[i].getName();
-            colQuantity.innerHTML = this._inventario[i].getQuantity();
-            colCost.innerHTML = this._inventario[i].getCost();
-            colTotal.innerHTML = this._inventario[i].getTotal();
+            col.innerHTML = aux._info
+            aux=aux._siguiente
         }
     }
     search(inId){
-        
-        let id = inId.value;
-        let pos = -1
-        for(let i=0; i<this._inventario.length; i++){
-            let s = this._inventario[i].getId()
-            if(s == id){
-                pos = i;
+        this._table.innerHTML="";
+        let tempo = this._inicio;
+        while(tempo!==null){
+            if(tempo._id == inId.value){
+                this._table.innerHTML=tempo._info
+                return tempo
+            } else {
+                tempo = tempo._siguiente
             }
         }
-        if(pos>=0){
-            this._table.innerHTML = "";
-            let row = this._table.insertRow(0);
-
-            let colId = row.insertCell(0);
-            let colName = row.insertCell(1);
-            let colQuantity = row.insertCell(2);
-            let colCost = row.insertCell(3);
-            let colTotal = row.insertCell(4);
-
-            colId.innerHTML = this._inventario[pos].getId();
-            colName.innerHTML = this._inventario[pos].getName();
-            colQuantity.innerHTML = this._inventario[pos].getQuantity();
-            colCost.innerHTML = this._inventario[pos].getCost();
-            colTotal.innerHTML = this._inventario[pos].getTotal();
-            
-            inId.value = "";
-            return alert("Articulo encontrado.");
-        }
-        else{
-            return alert("El articulo que busca no existe.");
-        }
+        return null;
     }
     delete(inId){
-        
-        let id = inId.value;
-        let pos = -1
-        for(let i=0; i<this._inventario.length; i++){
-            let s = this._inventario[i].getId()
-            if(s == id){
-                pos = i;
-            }
+        let search = this.search(inId)
+        if(search == null){
+            return null
         }
-        if(pos>=0){
-            this._table.innerHTML = "";
-            let row = this._table.insertRow(0);
-
-            let colId = row.insertCell(0);
-            let colName = row.insertCell(1);
-            let colQuantity = row.insertCell(2);
-            let colCost = row.insertCell(3);
-            let colTotal = row.insertCell(4);
-
-            colId.innerHTML = this._inventario[pos].getId();
-            colName.innerHTML = this._inventario[pos].getName();
-            colQuantity.innerHTML = this._inventario[pos].getQuantity();
-            colCost.innerHTML = this._inventario[pos].getCost();
-            colTotal.innerHTML = this._inventario[pos].getTotal();
-
-            let k = pos+1
-            for(let i = pos; i<this._inventario.length-1; i++,k++){
-                console.log(this._inventario)
-                let temp = this._inventario[i];
-                this._inventario[i] = this._inventario[k];
-                this._inventario[k] = temp;
-            }
-            
-            console.log(this._inventario)
-            this._inventario.pop();
-            inId.value = "";
-            return alert("Articulo eliminado exitosamente.");
+        else {
+            let del = null;
+        if (inId.value==this._inicio._id){
+            del = this._inicio
+            this._inicio = this._inicio._siguiente
+            del._siguiente = null
+            return del;
         }
-        else{
-            return alert("El articulo que desea eliminar no existe.");
+        let temp=this._inicio;
+        while(temp._siguiente != null ){
+        if (temp._siguiente._id==inId.value)
+        {
+            del=temp._siguiente;
+            temp._siguiente=temp._siguiente._siguiente;
+            del._siguiente=null;
+            return del;
+        } else
+            temp=temp._siguiente;
+        }
+        return del;
         }
     }
-    
+    addProductPosition(product,inPos){
+        let prdCount = this._count()
+        let search = this._find(product)
+        if(search == null && prdCount<20 && prdCount>inPos.value){
+            let count = 1;
+            let newProduct = false;
+        if (inPos.value==1){
+            newProduct = this._inicio
+            this._inicio = product
+            product._siguiente = newProduct
+            return true;
+        }
+        let temp=this._inicio;
+        while(count !== inPos ){
+            console.log(count)
+        if ((count)+1 == inPos.value)
+        {
+            newProduct=temp._siguiente;
+            temp._siguiente=product;
+            product._siguiente = newProduct
+            return true;
+        }else if(!temp){
+            return false
+        } else {
+            count++
+            temp=temp._siguiente;
+        }
+        }
+        return newProduct;
+        }
+        else {
+            return false
+        }
+    }
 }
